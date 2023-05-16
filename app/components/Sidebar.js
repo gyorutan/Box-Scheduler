6af60e68755fa184cb2cbadaacb5e28e5c1fe990
gyorutan <waou1998@gmail.com>
2023-05-17 08:41:57 +0900

    UPDATE

--- app/components/Sidebar.js
+++ app/components/Sidebar.js
@@ -37,49 +37,41 @@ const Sidebar = () => {
           </div>
         </Link>
       </div>
-      <div className="flex flex-col justify-between h-screen">
-        <div>
-          <Link href={"/home"}>
-            <div className="px-4 py-3 hover:bg-[#6dc4db] flex justify-center gap-2 items-center md:justify-end">
-              <span className="hidden lg:block">홈페이지</span>
-              <Image height="30" width="30" src="/images/home.png" alt="Logo" />
-            </div>
-          </Link>
-          <Link href={"/schedule"}>
-            <div className="px-4 py-3 hover:bg-[#6dc4db] flex justify-center gap-2 items-center md:justify-end">
-              <span className="hidden lg:block">스케쥴</span>
-              <Image height="30" width="30" src="/images/schedule.png" alt="Logo" />
-            </div>
-          </Link>
-        </div>
-        <div>
-          <Link href={`user/${currentUser._id}`}>
-            <div className="px-4 py-3 hover:bg-[#6dc4db] flex justify-center items-center">
-              <div className="hidden">
-                <p>{currentUser.username}</p>
-              </div>
-              <div className="">
-                <div className="rounded-full overflow-hidden h-9 w-9 md:h-11 md:w-11 lg:ml-28">
-                  <Image
-                    width={50}
-                    height={50}
-                    src={currentUser?.image || "/images/placeholder.jpg"}
-                    alt="Avatar"
-                  />
-                </div>
-              </div>
-            </div>
-          </Link>
-          <button
-            onClick={logout}
-            className="w-full px-4 py-3 hover:bg-[#6dc4db] flex justify-center md:justify-end items-center gap-2"
-          >
-            <span className="hidden md:block md:text-sm">로그아웃</span>
-            <div>
-              <HiArrowLeftOnRectangle size={35} />
-            </div>
-          </button>
-        </div>
+      <div>
+        <Link href={"/home"}>
+          <div className="px-4 py-3 hover:bg-[#6dc4db] flex justify-center gap-2 items-center md:justify-end">
+            <span className="hidden lg:block">홈페이지</span>
+            <Image height="30" width="30" src="/images/home.png" alt="Logo" />
+          </div>
+        </Link>
+        <Link href={"/schedule"}>
+          <div className="px-4 py-3 hover:bg-[#6dc4db] flex justify-center gap-2 items-center md:justify-end">
+            <span className="hidden lg:block">스케쥴</span>
+            <Image
+              height="30"
+              width="30"
+              src="/images/schedule.png"
+              alt="Logo"
+            />
+          </div>
+        </Link>
+      </div>
+      <div>
+      <Link href={`/user/${currentUser._id}`}>
+          <div className="px-4 py-3 hover:bg-[#6dc4db] flex justify-center gap-2 items-center md:justify-end">
+            <span className="hidden lg:block">{currentUser.username}</span>
+            <Image height="30" width="30" className="rounded-full" src={`${currentUser?.image || "/images/placeholder.jpg"}`} alt="Logo" />
+          </div>
+        </Link>
+        <button
+          onClick={logout}
+          className="w-full px-4 py-3 hover:bg-[#6dc4db] flex justify-center md:justify-end items-center gap-2"
+        >
+          <span className="hidden lg:block">로그아웃</span>
+          <div>
+            <HiArrowLeftOnRectangle size={35} />
+          </div>
+        </button>
       </div>
     </div>
   );
--- app/schedule/page.js
+++ app/schedule/page.js
@@ -4,7 +4,7 @@ import Layout from "../components/Layout";
 export default function Schedule() {
   return (
       <div className="p-3">
-        <div className="text-xl">스케쥴 내용</div>
+        <div className="text-xl">스케쥴 작성</div>
       </div>
   );
 }
